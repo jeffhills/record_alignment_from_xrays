4f34ac178f4a0fe581fb56cd69b6a92a3e01c7d0
Jeff Hills <jeffhills3@gmail.com>
2024-12-07 13:38:43 -0600

    test

--- app.R
+++ app.R
@@ -13,13 +13,13 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("testtestest"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
-                        "Number of bins:",
+                        "test:",
                         min = 1,
                         max = 50,
                         value = 30)
